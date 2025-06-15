42ef2827849ed019dbb0f5299689e3b130a3eded
dyloge <dylogedyloge@gmail.com>
2025-06-15 20:59:06 +0330

    chore:eslint exceptions for build-5

--- app/todos/[id]/page.tsx
+++ app/todos/[id]/page.tsx
@@ -7,14 +7,12 @@ import { getUserById } from "@/app/actions/users";
 // Enable revalidation every 60 seconds
 export const revalidate = 60;
 
-type Props = {
+interface PageProps {
   params: { id: string };
   searchParams: { [key: string]: string | string[] | undefined };
-};
+}
 
-export default async function TodoDetailPage(props: Props) {
-  const { params } = props;
-
+export default async function TodoDetailPage({ params }: PageProps) {
   // Fetch data on the server
   const [todoResult, userResult] = await Promise.all([
     getTodoById(params.id),
