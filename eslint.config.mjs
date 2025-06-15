5bd8268f545f5e6981b9a9ed0f22627a1ce7cd03
dyloge <dylogedyloge@gmail.com>
2025-06-15 20:37:00 +0330

    chore:eslint exceptions for build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,9 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-empty-object-type": "off"
-    }
-  }
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
