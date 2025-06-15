58de045fc09bc171e5e0e55c40cb53582abe0b04
dyloge <dylogedyloge@gmail.com>
2025-06-15 21:16:10 +0330

    chore:eslint exceptions for build-8

--- types/next.d.ts
+++
@@ -1,8 +0,0 @@
-import { Metadata } from 'next';
-
-declare module 'next' {
-  interface PageProps {
-    params: { [key: string]: string };
-    searchParams?: { [key: string]: string | string[] | undefined };
-  }
-} 
